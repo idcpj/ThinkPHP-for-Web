f516cc44775ba8ef5ea6e1eed123202163157fa1
陈鹏杰 <260083304@qq.com>
2017-03-14 20:04:35 +0800

    test

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -8,7 +8,9 @@ use Think\Controller;
 
 class IndexController extends Controller
 {
-    public function index(){
-        echo "1";
+    public function Index(){
+        return $this->display();
+        //var_dump(C());
     }
+
 }
---
+++ Application/Admin/Controller/LoginController.class.php
@@ -0,0 +1,15 @@
+<?php
+namespace Admin\Controller;
+
+use Think\Controller;
+
+class LoginController extends Controller
+{
+    public function index(){
+        return $this->display();
+    }
+
+    public function check(){
+
+    }
+}
